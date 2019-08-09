bf66f52ef0f9b58fac6e12d4bd075608cffcdd54
Kofi-611 <kofibond611@gmail.com>
2019-08-09 18:57:58 -0400

    first algebra one equation solver test

---
+++ real-website/operatorSymbols.js
@@ -0,0 +1,15 @@
+function subtractSymb(a){
+	document.getElementById(a).parentNode.firstChild.nodeValue = "&#8722;";
+}
+
+function divideSymb(b){
+	document.getElementById(b).parentNode.firstChild.nodeValue = "&#247;";
+}
+
+function multiplySymb(c){
+	document.getElementById(c).parentNode.firstChild.nodeValue = "&#215;";
+}
+
+function addSymb(d){
+	document.getElementById(d).parentNode.firstChild.nodeValue = "&#43;";
+}
