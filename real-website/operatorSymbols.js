0faf37f6a17213133274665c13ee99d9eeade03b
Kofi-611 <kofibond611@gmail.com>
2019-08-13 18:22:36 -0400

    algebra one equation solver dropdown fix

--- real-website/operatorSymbols.js
+++ real-website/operatorSymbols.js
@@ -1,15 +1,15 @@
 function subtractSymb(a){
-	document.getElementById(a).parentNode.firstChild.style.content = "\2212";
+	document.getElementById(a).style.content = "\2212";
 }
 
 function divideSymb(b){
-	document.getElementById(b).parentNode.firstChild.style.content = "\00F7";
+	document.getElementById(b).style.content = "\00F7";
 }
 
 function multiplySymb(c){
-	document.getElementById(c).parentNode.firstChild.style.content = "\00D7";
+	document.getElementById(c).style.content = "\00D7";
 }
 
 function addSymb(d){
-	document.getElementById(d).parentNode.firstChild.style.content = "\002B";
+	document.getElementById(d).style.content = "\002B";
 }
