de05c1935b385680e8f68be298d602f83031217e
Kofi-611 <kofibond611@gmail.com>
2019-09-03 10:00:47 -0400

    start of fix of equations

--- real-website/operatorSymbols.js
+++ real-website/operatorSymbols.js
@@ -1,15 +1,15 @@
-function subtractSymb(a){
-	document.getElementById(a).style.content = "\2212";
+function subtractSymb(){
+	document.getElementById("arithOperatorZero").style.content = "\2212";
 }
 
-function divideSymb(b){
+function divideSymb(){
 	document.getElementById(b).style.content = "\00F7";
 }
 
-function multiplySymb(c){
+function multiplySymb(){
 	document.getElementById(c).style.content = "\00D7";
 }
 
-function addSymb(d){
+function addSymb(){
 	document.getElementById(d).style.content = "\002B";
 }
