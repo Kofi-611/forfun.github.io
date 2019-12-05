1d664aba37c1c0da08acd3816ebb89a6215e900b
Kofi-611 <45661384+Kofi-611@users.noreply.github.com>
2019-12-05 12:49:27 -0500

    new sidebar item

--- real-website/maintop.js
+++ real-website/maintop.js
@@ -10,6 +10,18 @@ function accordion_one(){
 	}
 }
 
+function accordion_two(){
+	var c = document.getElementById("allAlgebra");
+	if (c.className.indexOf("w3-show") == -1){
+		c.className += " w3-show";
+		c.previousElementSibling.className += " w3-purple";
+	} else{
+		c.className = c.className.replace(" w3-show", "");
+		c.previousElementSibling.className =
+		c.previousElementSibling.className.replace(" w3-purple", "");
+	}
+}
+
 function sideopen(){
 	document.getElementById("allformulas").style.marginleft = "25%";
 	document.getElementById("sidenav").style.width = "25%";
